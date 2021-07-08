37f898ea588bb786497478267e1538faf95a3733
Doudou <sowabdou2009@gmail.com>
2021-07-08 15:01:35 +0200

    affiche payementAdm et ajoute admin

--- src/views/Admin.jsx
+++ src/views/Admin.jsx
@@ -1,11 +1,18 @@
-import axios from "axios";
 import { useState } from "react";
-import { useHistory } from "react-router-dom";
 import Modal from 'react-modal'
 import AfficheUsersAdm from "../components/AficheUsersAdm";
 import AffichePayementAdm from "../components/AffichePayementAdm";
+import Button from '@material-ui/core/Button';
+import axios from "axios";
 
 function Admin() {
+
+    const [name, setName] = useState("")
+    const [surname, setSurname] = useState("")
+    const [telephone, setTelephone] = useState("")
+    const [role, setRole] = useState("")
+    const [password, setPassword] = useState("")
+    const [confirmPassword, setConfirmPassword] = useState("")
 
     const [modalIsOpen, setIsOpen] = useState(false);
     const customStyles = {
@@ -22,25 +29,96 @@ function Admin() {
         setIsOpen(true)
     }
 
+    const valideAjout = () => {
+        // console.log(" name:",name);
+        // console.log(" surname:",surname);
+        // console.log(" telephone:",parseInt(telephone));
+        // console.log(" role:",parseInt(role));
+        // console.log(" passWord:",password);
+        // console.log(" confirmPassword:",confirmPassword);
+
+        try {
+            const infoAdmin = {
+                firstname: name,
+                surname: surname,
+                role:role,
+                password: password,
+                telephone: parseInt(telephone)
+            }
+            if (password === confirmPassword) {
+
+                const response = axios.post("http://localhost:9001/adminsignup", infoAdmin)
+                
+
+                    console.log("response valide ajout admin :", response);
+                    alert("you can connect now !")
+                    // history.push("/Connexion")
+                    setIsOpen(false)
+                
+
+            } else {
+
+                alert("confirm pasetconfirmPasswordssword not correct")
+            }
+
+        } catch (error) {
+            console.log("error :", error);
+
+        }
+    }
+
     return (
         <div className="container text-center">
-            <div className="row">
+            <div className="row" style={{ height: 100 }}>
                 <div className="col-6 offset-3">
-
-                    <button onClick={ajouteAdmin}>ajoute admin</button>
+                    <Button variant="contained" color="primary" disableElevation onClick={ajouteAdmin}>
+                        ajoute admin
+                    </Button>
                 </div>
 
             </div>
             <div className="row">
-                <div className="col-5 border border-dark">
+                <div className="col-5 ">
                     <AfficheUsersAdm />
                 </div>
-                <div className="col-5 offset-2 border border-dark">
+                <div className="col-5 offset-2 ">
                     <AffichePayementAdm />
                 </div>
             </div>
             <Modal isOpen={modalIsOpen} style={customStyles} >
-                <button onClick={()=>setIsOpen(false)}>sortir</button>
+                <div>
+                    <div className="mb-3">
+                        <label className="form-label">name</label>
+                        <input type="text" className="form-control" autoComplete="name" onChange={(e) => setName(e.target.value)}></input>
+                    </div>
+                    <div className="mb-3">
+                        <label for="exampleInputEmail1" className="form-label">surname</label>
+                        <input type="text" className="form-control" onChange={(e) => setSurname(e.target.value)}></input>
+                    </div>
+                    <div className="mb-3">
+                        <label for="exampleInputEmail1" className="form-label">telephone</label>
+                        <input type="text" className="form-control" onChange={(e) => setTelephone(e.target.value)}></input>
+                    </div>
+                    <div class="input-group mb-3" onChange={(e) => setRole(e.target.value)} >
+                        <label class="input-group-text" for="inputGroupSelect01">role</label>
+                        <select class="form-select" id="inputGroupSelect01">
+                            <option value="admin">Admin</option>
+                            <option value="agent">Agent</option>
+                        </select>
+                    </div>
+                    <div className="mb-3">
+                        <label for="exampleFormControlInput1" className="form-label">Password </label>
+                        <input type="Password" className="form-control" onChange={(e) => setPassword(e.target.value)}></input>
+                    </div>
+                    <div className="mb-3">
+                        <label for="exampleFormControlInput1" className="form-label">confirmPassword</label>
+                        <input type="Password" className="form-control" onChange={(e) => setConfirmPassword(e.target.value)}></input>
+                    </div>
+                </div>
+                <button type="button" class="btn btn-danger" onClick={() => setIsOpen(false)}>annuler</button>
+                <button type="button" class="btn btn-success" onClick={valideAjout}>valider</button>
+
+
             </Modal>
 
 
